fde9e6c6e53f9db8ef225a42a92b518a12a773d9
Alyssa Broussard <alyssabroussard@legalshieldcorp.com>
2017-01-24 13:37:35 -0600

    Assignment 17 complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,30 @@
 require 'random_data'
 
- 50.times do
-   Post.create!(
-     title:  RandomData.random_sentence,
-     body:   RandomData.random_paragraph
-   )
- end
- posts = Post.all
+50.times do
+  Post.create!(
+    title:  RandomData.random_sentence,
+    body:   RandomData.random_paragraph
+  )
+end
+posts = Post.all
 
- 100.times do
-   Comment.create!(
-     post: posts.sample,
-     body: RandomData.random_paragraph
-   )
- end
+100.times do
+  Comment.create!(
+    post: posts.sample,
+    body: RandomData.random_paragraph
+  )
+end
 
- puts "Seed finished"
- puts "#{Post.count} posts created"
- puts "#{Comment.count} comments created"
+unique_post = Post.find_or_create_by!(
+  title: "I'm a unique title!",
+  body: 'with a unique body!'
+)
+
+Comment.find_or_create_by!(
+  post: unique_post,
+  body: "I'm a unique comment! Just look at me go!"
+)
+
+puts 'Seed finished'
+puts "#{Post.count} posts created"
+puts "#{Comment.count} comments created"
