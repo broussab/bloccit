97674b86806dffd548e4169e4c1fa7410a7b7b7e
Alyssa Broussard <alyssabroussard@legalshieldcorp.com>
2017-01-27 14:21:49 -0600

    Assignment 20 completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,29 @@
 require 'random_data'
 
- 50.times do
-   Post.create!(
-     title:  RandomData.random_sentence,
-     body:   RandomData.random_paragraph
-   )
- end
- posts = Post.all
+50.times do
+  Post.create!(
+    title:  RandomData.random_sentence,
+    body:   RandomData.random_paragraph
+  )
+end
+posts = Post.all
 
- 100.times do
-   Comment.create!(
-     post: posts.sample,
-     body: RandomData.random_paragraph
-   )
- end
+100.times do
+  Comment.create!(
+    post: posts.sample,
+    body: RandomData.random_paragraph
+  )
+end
 
- puts "Seed finished"
- puts "#{Post.count} posts created"
- puts "#{Comment.count} comments created"
+35.times do
+  Question.create!(
+    title: RandomData.random_question,
+    body: RandomData.random_paragraph,
+    resolved: true
+  )
+end
+
+puts 'Seed finished'
+puts "#{Post.count} posts created"
+puts "#{Comment.count} comments created"
+puts "#{Question.count} questions created"
