6f236bab392ecb5d284877414d264acb474ce154
Alyssa Broussard <alyssabroussard@legalshieldcorp.com>
2017-02-03 16:04:25 -0600

    Assignment 24 completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ApplicationRecord
-  before_save { self.email = email.downcase if email.present? }
+  before_save do
+    nameArray = name.to_s.split(' ')
+    nameArray.each(&:capitalize!)
+    self.name = nameArray.join(' ')
+
+    self.email = email.downcase if email.present?
+  end
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
   validates :password, presence: true, length: { minimum: 6 }, if: 'password_digest.nil?'
   validates :password, length: { minimum: 6 }, allow_blank: true
