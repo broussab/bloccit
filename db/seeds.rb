574e15267b56edba2755e28bd51bf80b3197fe73
Alyssa Broussard <alyssabroussard@legalshieldcorp.com>
2017-02-08 11:37:17 -0600

    Checkpoint 27 completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,14 @@
 require 'random_data'
+5.times do
+  User.create!(
+    # #3
+    name:     RandomData.random_name,
+    email:    RandomData.random_email,
+    password: RandomData.random_sentence
+  )
+end
+users = User.all
+
 15.times do
   Topic.create!(
     name:         RandomData.random_sentence,
@@ -9,7 +19,8 @@ topics = Topic.all
 
 50.times do
   Post.create!(
-    topic: topics.sample,
+    user:   users.sample,
+    topic:  topics.sample,
     title:  RandomData.random_sentence,
     body:   RandomData.random_paragraph
   )
@@ -31,7 +42,14 @@ end
   )
 end
 
+user = User.first
+user.update_attributes!(
+  email: 'alybeic@gmail.com', # replace this with your personal email
+  password: 'helloworld'
+)
+
 puts 'Seed finished'
+puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
